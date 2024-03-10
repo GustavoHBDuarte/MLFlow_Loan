ff268248354b519c5a8c60fb92ec5df57af89622
Gustavo Duarte <gustavo.hduarte@hsl.org.br>
2024-03-10 20:08:29 -0300

    Atualizdo arquivo 'data_transformation.py'. Incluido try/except para a verificacao da presenca/ausencia da coluna target no dataframe.

--- project/src/data/data_transformation.py
+++ project/src/data/data_transformation.py
@@ -24,25 +24,59 @@ class DataTransformation:
 
     def __init__(self, dataframe: pd.DataFrame):
         
-        # Loading yaml config file
-        yaml_file = load_config_file()
+        
+        try:
+            # Loading yaml config file
+            yaml_file = load_config_file()
+
+            if not yaml_file:
+                raise ValueError('Failed do load YAM config file')
+            
+        except Exception as e:
+            logger.error(f'Error loading config file: {e}')
+            raise   
         
         self.dataframe = dataframe
         self.target_variable = yaml_file.get('target_variable')
 
     def train_test_data_split(self):
 
-        # Loading yaml config file
-        yaml_file = load_config_file()
+        try:
+            # Loading yaml config file
+            yaml_file = load_config_file()
 
-        X = self.dataframe.drop(columns=[self.target_variable])
-        y = self.dataframe[self.target_variable]
+            if not yaml_file:
+                raise ValueError('Failed do load YAML config file')
 
-        X_train, X_val, y_train, y_val = train_test_split(X,
-                                                          y,
-                                                          stratify=y,
-                                                          test_size=yaml_file.get('test_size'),
-                                                          random_state=yaml_file.get('random_state')
-                                                          )
+        except Exception as e:
+            logger.error(f'Error loading config file: {e}')
+            raise
+
+
+        try:
+            # Check if target variable is present in the dataframe
+            if self.target_variable not in self.dataframe.columns:
+                raise ValueError(f"Target variable '{self.target_variable}' not found in the dataframe")
+
+            logger.info(f"Target '{self.target_variable}' variable found in the dataframe. Performing X, y split and train/val split")
+            
+            X = self.dataframe.drop(columns=[self.target_variable])
+            y = self.dataframe[self.target_variable]
+
+            # Perform split
+            X_train, X_val, y_train, y_val = train_test_split(X,
+                                                              y,
+                                                              stratify=y,
+                                                              test_size=yaml_file.get('test_size'),
+                                                              random_state=yaml_file.get('random_state')
+                                                              )
+            
+            logger.info(f'Data split performed successfully!')
+
+            
+
+        except Exception as e:
+            logger.error(f'Error during data split: {e}')
+            raise
 
         return X_train, X_val, y_train, y_val 
